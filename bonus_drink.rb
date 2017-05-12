0a0588a44ba9bf8d03a4e2df6ee874268a148fef
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2017-05-12 16:58:04 +0900

    トータル本数算出機能を実装

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,19 @@
 class BonusDrink
-  def self.total_count_for(amount)
-    0
+  def self.total_count_for(purchase)
+    purchase + calculate_bonus(purchase)
+  end
+
+  private
+
+  def self.calculate_bonus(purchase)
+    bonus = 0
+
+    if purchase >= 3
+      purchase -= 3
+      bonus += 1
+      bonus = bonus + (purchase / 2) if purchase > 0
+    end
+
+    bonus
   end
 end
